f5013b03fe762febe90a3e07c9643842b781e3ed
pldmgg <testadmin@test2.lab>
2017-01-04 14:11:18 -0500

    Initial commit for Check-SameObject.ps1

---
+++ Check-SameObject.ps1
@@ -0,0 +1,125 @@
+function Check-SameObject {
+    [CmdletBinding()]
+    Param( 
+        [Parameter(Mandatory=$False)]
+        $VariableNameA = $(Read-Host -Prompt "Please enter the name of the first variable you would like to compare"),
+
+        [Parameter(Mandatory=$False)]
+        $VariableNameB = $(Read-Host -Prompt "Please enter the name of the second variable you would like to compare")
+    )
+
+    $ArrayOfShadowVariablesPrep = Get-Variable| Where-Object {
+        $_.Name -ne "`$" -and
+        $_.Name -ne "`?" -and
+        $_.Name -ne "`^" -and
+        $_.Name -notlike "*Shadow"
+    }
+
+    $ArrayOfShadowVariables = @()
+    $ArrayOfShadowVariablesPrep | foreach {
+        try
+        {
+            $(Get-Variable -Name "$($_.Name)" -ValueOnly).GetHashCode() | Out-Null
+        }
+        catch [System.Management.Automation.RuntimeException]
+        {
+            $HashCodeDNE = "$($_.Name) HashCode does not exist"
+            Write-Verbose $HashCodeDNE
+        }
+        if (!$HashCodeDNE) {
+            New-Variable -Name "$($_.Name)Shadow" -Scope Global -Value $(
+                New-Object PSObject -Property @{
+                    VariableName        = "$($_.Name)"
+                    VariableHashCode    = "$($(Get-Variable -Name "$($_.Name)" -ValueOnly).GetHashCode())"
+                    VariableValue       = "$($_.Value)"
+                }
+            ) -Force
+            $ArrayOfShadowVariables +=, $(Get-Variable -Name "$($_.Name)Shadow" -ValueOnly)
+        }
+        if ($HashCodeDNE) {
+            Remove-Variable -Name "HashCodeDNE"
+        }
+    }
+
+    $SameObjectHashCode = $($ArrayOfShadowVariables.VariableHashCode | Group-Object | Where-Object {$_.Count -gt 1}).Name
+
+    $SameObjects = foreach ($obj1 in $SameObjectHashCode) {
+        $ArrayOfShadowVariables | Where-Object {$_.VariableHashCode -eq $obj1}
+    }
+
+    # Return True if VariableName1 and VariableName2 have the same HashCode
+    $($SameObjects | Where-Object {$_.Name -eq "$VariableNameA"}).VariableHashCode -eq $($SameObjects | Where-Object {$_.Name -eq "$VariableNameB"}).VariableHashCode
+
+    # Cleanup
+    $ShadowVariables = Get-Variable | Where-Object {$_.Name -like "*Shadow"}
+    foreach ($shadowvariable in $ShadowVariables) {
+        Remove-Variable -Name "$($shadowvariable.Name)" -Scope Global -Force
+    }
+}
+# SIG # Begin signature block
+# MIIMLAYJKoZIhvcNAQcCoIIMHTCCDBkCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQU8+8+A5RNlvKFdALBv8LKTMwk
+# Kl2gggmhMIID/jCCAuagAwIBAgITawAAAAQpgJFit9ZYVQAAAAAABDANBgkqhkiG
+# 9w0BAQsFADAwMQwwCgYDVQQGEwNMQUIxDTALBgNVBAoTBFpFUk8xETAPBgNVBAMT
+# CFplcm9EQzAxMB4XDTE1MDkwOTA5NTAyNFoXDTE3MDkwOTEwMDAyNFowPTETMBEG
+# CgmSJomT8ixkARkWA0xBQjEUMBIGCgmSJomT8ixkARkWBFpFUk8xEDAOBgNVBAMT
+# B1plcm9TQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCmRIzy6nwK
+# uqvhoz297kYdDXs2Wom5QCxzN9KiqAW0VaVTo1eW1ZbwZo13Qxe+6qsIJV2uUuu/
+# 3jNG1YRGrZSHuwheau17K9C/RZsuzKu93O02d7zv2mfBfGMJaJx8EM4EQ8rfn9E+
+# yzLsh65bWmLlbH5OVA0943qNAAJKwrgY9cpfDhOWiYLirAnMgzhQd3+DGl7X79aJ
+# h7GdVJQ/qEZ6j0/9bTc7ubvLMcJhJCnBZaFyXmoGfoOO6HW1GcuEUwIq67hT1rI3
+# oPx6GtFfhCqyevYtFJ0Typ40Ng7U73F2hQfsW+VPnbRJI4wSgigCHFaaw38bG4MH
+# Nr0yJDM0G8XhAgMBAAGjggECMIH/MBAGCSsGAQQBgjcVAQQDAgEAMB0GA1UdDgQW
+# BBQ4uUFq5iV2t7PneWtOJALUX3gTcTAZBgkrBgEEAYI3FAIEDB4KAFMAdQBiAEMA
+# QTAOBgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAfBgNVHSMEGDAWgBR2
+# lbqmEvZFA0XsBkGBBXi2Cvs4TTAxBgNVHR8EKjAoMCagJKAihiBodHRwOi8vcGtp
+# L2NlcnRkYXRhL1plcm9EQzAxLmNybDA8BggrBgEFBQcBAQQwMC4wLAYIKwYBBQUH
+# MAKGIGh0dHA6Ly9wa2kvY2VydGRhdGEvWmVyb0RDMDEuY3J0MA0GCSqGSIb3DQEB
+# CwUAA4IBAQAUFYmOmjvbp3goa3y95eKMDVxA6xdwhf6GrIZoAg0LM+9f8zQOhEK9
+# I7n1WbUocOVAoP7OnZZKB+Cx6y6Ek5Q8PeezoWm5oPg9XUniy5bFPyl0CqSaNWUZ
+# /zC1BE4HBFF55YM0724nBtNYUMJ93oW/UxsWL701c3ZuyxBhrxtlk9TYIttyuGJI
+# JtbuFlco7veXEPfHibzE+JYc1MoGF/whz6l7bC8XbgyDprU1JS538gbgPBir4RPw
+# dFydubWuhaVzRlU3wedYMsZ4iejV2xsf8MHF/EHyc/Ft0UnvcxBqD0sQQVkOS82X
+# +IByWP0uDQ2zOA1L032uFHHA65Bt32w8MIIFmzCCBIOgAwIBAgITWAAAADw2o858
+# ZSLnRQAAAAAAPDANBgkqhkiG9w0BAQsFADA9MRMwEQYKCZImiZPyLGQBGRYDTEFC
+# MRQwEgYKCZImiZPyLGQBGRYEWkVSTzEQMA4GA1UEAxMHWmVyb1NDQTAeFw0xNTEw
+# MjcxMzM1MDFaFw0xNzA5MDkxMDAwMjRaMD4xCzAJBgNVBAYTAlVTMQswCQYDVQQI
+# EwJWQTEPMA0GA1UEBxMGTWNMZWFuMREwDwYDVQQDEwhaZXJvQ29kZTCCASIwDQYJ
+# KoZIhvcNAQEBBQADggEPADCCAQoCggEBAJ8LM3f3308MLwBHi99dvOQqGsLeC11p
+# usrqMgmEgv9FHsYv+IIrW/2/QyBXVbAaQAt96Tod/CtHsz77L3F0SLuQjIFNb522
+# sSPAfDoDpsrUnZYVB/PTGNDsAs1SZhI1kTKIjf5xShrWxo0EbDG5+pnu5QHu+EY6
+# irn6C1FHhOilCcwInmNt78Wbm3UcXtoxjeUl+HlrAOxG130MmZYWNvJ71jfsb6lS
+# FFE6VXqJ6/V78LIoEg5lWkuNc+XpbYk47Zog+pYvJf7zOric5VpnKMK8EdJj6Dze
+# 4tJ51tDoo7pYDEUJMfFMwNOO1Ij4nL7WAz6bO59suqf5cxQGd5KDJ1ECAwEAAaOC
+# ApEwggKNMA4GA1UdDwEB/wQEAwIHgDA9BgkrBgEEAYI3FQcEMDAuBiYrBgEEAYI3
+# FQiDuPQ/hJvyeYPxjziDsLcyhtHNeIEnofPMH4/ZVQIBZAIBBTAdBgNVHQ4EFgQU
+# a5b4DOy+EUyy2ILzpUFMmuyew40wHwYDVR0jBBgwFoAUOLlBauYldrez53lrTiQC
+# 1F94E3EwgeMGA1UdHwSB2zCB2DCB1aCB0qCBz4aBq2xkYXA6Ly8vQ049WmVyb1ND
+# QSxDTj1aZXJvU0NBLENOPUNEUCxDTj1QdWJsaWMlMjBLZXklMjBTZXJ2aWNlcyxD
+# Tj1TZXJ2aWNlcyxDTj1Db25maWd1cmF0aW9uLERDPXplcm8sREM9bGFiP2NlcnRp
+# ZmljYXRlUmV2b2NhdGlvbkxpc3Q/YmFzZT9vYmplY3RDbGFzcz1jUkxEaXN0cmli
+# dXRpb25Qb2ludIYfaHR0cDovL3BraS9jZXJ0ZGF0YS9aZXJvU0NBLmNybDCB4wYI
+# KwYBBQUHAQEEgdYwgdMwgaMGCCsGAQUFBzAChoGWbGRhcDovLy9DTj1aZXJvU0NB
+# LENOPUFJQSxDTj1QdWJsaWMlMjBLZXklMjBTZXJ2aWNlcyxDTj1TZXJ2aWNlcyxD
+# Tj1Db25maWd1cmF0aW9uLERDPXplcm8sREM9bGFiP2NBQ2VydGlmaWNhdGU/YmFz
+# ZT9vYmplY3RDbGFzcz1jZXJ0aWZpY2F0aW9uQXV0aG9yaXR5MCsGCCsGAQUFBzAC
+# hh9odHRwOi8vcGtpL2NlcnRkYXRhL1plcm9TQ0EuY3J0MBMGA1UdJQQMMAoGCCsG
+# AQUFBwMDMBsGCSsGAQQBgjcVCgQOMAwwCgYIKwYBBQUHAwMwDQYJKoZIhvcNAQEL
+# BQADggEBACbc1NDl3NTMuqFwTFd8NHHCsSudkVhuroySobzUaFJN2XHbdDkzquFF
+# 6f7KFWjqR3VN7RAi8arW8zESCKovPolltpp3Qu58v59qZLhbXnQmgelpA620bP75
+# zv8xVxB9/xmmpOHNkM6qsye4IJur/JwhoHLGqCRwU2hxP1pu62NUK2vd/Ibm8c6w
+# PZoB0BcC7SETNB8x2uKzJ2MyAIuyN0Uy/mGDeLyz9cSboKoG6aQibnjCnGAVOVn6
+# J7bvYWJsGu7HukMoTAIqC6oMGerNakhOCgrhU7m+cERPkTcADVH/PWhy+FJWd2px
+# ViKcyzWQSyX93PcOj2SsHvi7vEAfCGcxggH1MIIB8QIBATBUMD0xEzARBgoJkiaJ
+# k/IsZAEZFgNMQUIxFDASBgoJkiaJk/IsZAEZFgRaRVJPMRAwDgYDVQQDEwdaZXJv
+# U0NBAhNYAAAAPDajznxlIudFAAAAAAA8MAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3
+# AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisG
+# AQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBSaM+VvJpJw
+# 13TXaUeLnR9s17wS3jANBgkqhkiG9w0BAQEFAASCAQAn5jgWA4MJ8Gew6Ik60Eqm
+# awfxtNhCyOgFKBhvIJC2GQ2b3vKis9wExLo2fhElgnHnToj3bGdH4H4C0dr7s6vm
+# jTLxbvp6VPQOsl65xIG7zuCLsFel2ZpGM7ZrukD606bCeLrTJv9wSXtuCVj70MQ1
+# ObxgboKuLUTf28TmqGHsiVQtc91vwFHKz/PrJoCQVNYcTwDK2S+LLzAxWBApsfoG
+# EZWwKkTjjiv/QCUvdGmz4isg6s6isgEBNRtFMH9op1YcucZxIdfh82fCcqpluorf
+# 9/ZXuMWoClAOOSgQJ6BGb2DrAgtNNVYW6Chktxckd5WuBFpp1a5QrDCtB/rFJHFe
+# SIG # End signature block
