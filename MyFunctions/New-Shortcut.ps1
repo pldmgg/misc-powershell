f6607ae5675310c31617ad30f93b6ff914765e97
pldmgg <pldmgg@mykolab.com>
2017-08-07 14:49:15 -0400

    Update New-Shortcut

---
+++ MyFunctions/New-Shortcut.ps1
@@ -0,0 +1,168 @@
+function New-Shortcut {
+    [CmdletBinding()]
+    Param( 
+        [Parameter(Mandatory=$False)]
+        $AppLocation = $(Read-Host -Prompt "Please enter the full path to the file you would like to create a shortcut to"),
+
+        [Parameter(Mandatory=$False)]
+        $ShortcutDestDir = [System.Environment]::CurrentDirectory,
+
+        [Parameter(Mandatory=$False)]
+        $ShortcutFileName = $(Read-Host -Prompt "Please enter the file name you would like the new shortcut to have"),
+
+        [Parameter(Mandatory=$False)]
+        $ShortcutIconFilePath, # path to a .ico file for the shortcut icon
+
+        [Parameter(Mandatory=$False)]
+        [string]$ShortcutArguments
+
+    )
+    
+    ##### BEGIN Native Helper Functions #####
+
+    function Get-Elevation {
+        [System.Security.Principal.WindowsPrincipal]currentPrincipal = New-Object System.Security.Principal.WindowsPrincipal(
+            [System.Security.Principal.WindowsIdentity]::GetCurrent()
+        )
+    
+        [System.Security.Principal.WindowsBuiltInRole]administratorsRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+    
+        if(currentPrincipal.IsInRole(administratorsRole)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    
+    ##### END Native Helper Functions #####
+
+    ##### BEGIN Variable/Parameter Transforms and PreRun Prep #####
+
+    if (!$(Get-Elevation)) {
+        Write-Error "The New-Shortcut cmdlet must be run from an elevated shell (i.e. Run As Administrator)! Halting!"
+        $global:FunctionResult = "1"
+        return
+    }
+
+    if (! $(Test-Path $ShortcutDestDir)) {
+        Write-Error "The path $ShortcutDestDir was not found! Halting!"
+        $global:FunctionResult = "1"
+        return
+    }
+
+    $InvalidFileNameChars = [System.IO.Path]::GetInvalidFileNameChars()
+    $InvalidFileNameChars = $InvalidFileNameChars | foreach {if ($_ -eq '\') {'\\'} else {$_}}
+
+    [System.Collections.ArrayList]$FoundIllegalCharacters = @()
+    $InvalidFileNameChars | foreach {
+        if ($ShortcutFileName -match $_) {
+
+            Write-Error "The filename for the shortcut (i.e. $ShortcutFileName) contains an illegal character '$_' ! Halting!"
+            $global:FunctionResult = "1"
+            return
+        }
+    }
+    if ($ShortcutFileName -match $InvalidFileNameChars) {
+
+    }
+    if ($ShortcutFileName -match "\.lnk$") {
+        $ShortcutFileName = $($ShortcutFileName -split "\.lnk")[0]
+    }
+
+    ##### END Variable/Parameter Transforms and PreRun Prep #####
+
+
+    ##### BEGIN Main Body #####
+
+    $WshShell = New-Object -ComObject WScript.Shell
+    # This is where the shortcut will be created
+    $Shortcut = $WshShell.CreateShortcut("$ShortcutDestDir\$ShortcutFileName.lnk")
+    # This is the program the shortcut will open
+    $Shortcut.TargetPath = "$AppLocation"
+    # This is the icon location that the shortcut will use
+    $Shortcut.IconLocation = "$ShortcutIconFilePath,0"
+    # This is any extra parameters that the shortcut may have. For example, opening to google.com when internet explorer opens
+    $Shortcut.Arguments = "$ShortcutArguments"
+    # This command will save all the modifications to the newly created shortcut.
+    $Shortcut.Save()
+
+    ##### END Main Body #####
+
+}
+
+
+
+
+
+
+
+
+# SIG # Begin signature block
+# MIIMLAYJKoZIhvcNAQcCoIIMHTCCDBkCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUYjinvug3tGyya1T644hcwXHq
+# SvigggmhMIID/jCCAuagAwIBAgITawAAAAQpgJFit9ZYVQAAAAAABDANBgkqhkiG
+# 9w0BAQsFADAwMQwwCgYDVQQGEwNMQUIxDTALBgNVBAoTBFpFUk8xETAPBgNVBAMT
+# CFplcm9EQzAxMB4XDTE1MDkwOTA5NTAyNFoXDTE3MDkwOTEwMDAyNFowPTETMBEG
+# CgmSJomT8ixkARkWA0xBQjEUMBIGCgmSJomT8ixkARkWBFpFUk8xEDAOBgNVBAMT
+# B1plcm9TQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCmRIzy6nwK
+# uqvhoz297kYdDXs2Wom5QCxzN9KiqAW0VaVTo1eW1ZbwZo13Qxe+6qsIJV2uUuu/
+# 3jNG1YRGrZSHuwheau17K9C/RZsuzKu93O02d7zv2mfBfGMJaJx8EM4EQ8rfn9E+
+# yzLsh65bWmLlbH5OVA0943qNAAJKwrgY9cpfDhOWiYLirAnMgzhQd3+DGl7X79aJ
+# h7GdVJQ/qEZ6j0/9bTc7ubvLMcJhJCnBZaFyXmoGfoOO6HW1GcuEUwIq67hT1rI3
+# oPx6GtFfhCqyevYtFJ0Typ40Ng7U73F2hQfsW+VPnbRJI4wSgigCHFaaw38bG4MH
+# Nr0yJDM0G8XhAgMBAAGjggECMIH/MBAGCSsGAQQBgjcVAQQDAgEAMB0GA1UdDgQW
+# BBQ4uUFq5iV2t7PneWtOJALUX3gTcTAZBgkrBgEEAYI3FAIEDB4KAFMAdQBiAEMA
+# QTAOBgNVHQ8BAf8EBAMCAYYwDwYDVR0TAQH/BAUwAwEB/zAfBgNVHSMEGDAWgBR2
+# lbqmEvZFA0XsBkGBBXi2Cvs4TTAxBgNVHR8EKjAoMCagJKAihiBodHRwOi8vcGtp
+# L2NlcnRkYXRhL1plcm9EQzAxLmNybDA8BggrBgEFBQcBAQQwMC4wLAYIKwYBBQUH
+# MAKGIGh0dHA6Ly9wa2kvY2VydGRhdGEvWmVyb0RDMDEuY3J0MA0GCSqGSIb3DQEB
+# CwUAA4IBAQAUFYmOmjvbp3goa3y95eKMDVxA6xdwhf6GrIZoAg0LM+9f8zQOhEK9
+# I7n1WbUocOVAoP7OnZZKB+Cx6y6Ek5Q8PeezoWm5oPg9XUniy5bFPyl0CqSaNWUZ
+# /zC1BE4HBFF55YM0724nBtNYUMJ93oW/UxsWL701c3ZuyxBhrxtlk9TYIttyuGJI
+# JtbuFlco7veXEPfHibzE+JYc1MoGF/whz6l7bC8XbgyDprU1JS538gbgPBir4RPw
+# dFydubWuhaVzRlU3wedYMsZ4iejV2xsf8MHF/EHyc/Ft0UnvcxBqD0sQQVkOS82X
+# +IByWP0uDQ2zOA1L032uFHHA65Bt32w8MIIFmzCCBIOgAwIBAgITWAAAADw2o858
+# ZSLnRQAAAAAAPDANBgkqhkiG9w0BAQsFADA9MRMwEQYKCZImiZPyLGQBGRYDTEFC
+# MRQwEgYKCZImiZPyLGQBGRYEWkVSTzEQMA4GA1UEAxMHWmVyb1NDQTAeFw0xNTEw
+# MjcxMzM1MDFaFw0xNzA5MDkxMDAwMjRaMD4xCzAJBgNVBAYTAlVTMQswCQYDVQQI
+# EwJWQTEPMA0GA1UEBxMGTWNMZWFuMREwDwYDVQQDEwhaZXJvQ29kZTCCASIwDQYJ
+# KoZIhvcNAQEBBQADggEPADCCAQoCggEBAJ8LM3f3308MLwBHi99dvOQqGsLeC11p
+# usrqMgmEgv9FHsYv+IIrW/2/QyBXVbAaQAt96Tod/CtHsz77L3F0SLuQjIFNb522
+# sSPAfDoDpsrUnZYVB/PTGNDsAs1SZhI1kTKIjf5xShrWxo0EbDG5+pnu5QHu+EY6
+# irn6C1FHhOilCcwInmNt78Wbm3UcXtoxjeUl+HlrAOxG130MmZYWNvJ71jfsb6lS
+# FFE6VXqJ6/V78LIoEg5lWkuNc+XpbYk47Zog+pYvJf7zOric5VpnKMK8EdJj6Dze
+# 4tJ51tDoo7pYDEUJMfFMwNOO1Ij4nL7WAz6bO59suqf5cxQGd5KDJ1ECAwEAAaOC
+# ApEwggKNMA4GA1UdDwEB/wQEAwIHgDA9BgkrBgEEAYI3FQcEMDAuBiYrBgEEAYI3
+# FQiDuPQ/hJvyeYPxjziDsLcyhtHNeIEnofPMH4/ZVQIBZAIBBTAdBgNVHQ4EFgQU
+# a5b4DOy+EUyy2ILzpUFMmuyew40wHwYDVR0jBBgwFoAUOLlBauYldrez53lrTiQC
+# 1F94E3EwgeMGA1UdHwSB2zCB2DCB1aCB0qCBz4aBq2xkYXA6Ly8vQ049WmVyb1ND
+# QSxDTj1aZXJvU0NBLENOPUNEUCxDTj1QdWJsaWMlMjBLZXklMjBTZXJ2aWNlcyxD
+# Tj1TZXJ2aWNlcyxDTj1Db25maWd1cmF0aW9uLERDPXplcm8sREM9bGFiP2NlcnRp
+# ZmljYXRlUmV2b2NhdGlvbkxpc3Q/YmFzZT9vYmplY3RDbGFzcz1jUkxEaXN0cmli
+# dXRpb25Qb2ludIYfaHR0cDovL3BraS9jZXJ0ZGF0YS9aZXJvU0NBLmNybDCB4wYI
+# KwYBBQUHAQEEgdYwgdMwgaMGCCsGAQUFBzAChoGWbGRhcDovLy9DTj1aZXJvU0NB
+# LENOPUFJQSxDTj1QdWJsaWMlMjBLZXklMjBTZXJ2aWNlcyxDTj1TZXJ2aWNlcyxD
+# Tj1Db25maWd1cmF0aW9uLERDPXplcm8sREM9bGFiP2NBQ2VydGlmaWNhdGU/YmFz
+# ZT9vYmplY3RDbGFzcz1jZXJ0aWZpY2F0aW9uQXV0aG9yaXR5MCsGCCsGAQUFBzAC
+# hh9odHRwOi8vcGtpL2NlcnRkYXRhL1plcm9TQ0EuY3J0MBMGA1UdJQQMMAoGCCsG
+# AQUFBwMDMBsGCSsGAQQBgjcVCgQOMAwwCgYIKwYBBQUHAwMwDQYJKoZIhvcNAQEL
+# BQADggEBACbc1NDl3NTMuqFwTFd8NHHCsSudkVhuroySobzUaFJN2XHbdDkzquFF
+# 6f7KFWjqR3VN7RAi8arW8zESCKovPolltpp3Qu58v59qZLhbXnQmgelpA620bP75
+# zv8xVxB9/xmmpOHNkM6qsye4IJur/JwhoHLGqCRwU2hxP1pu62NUK2vd/Ibm8c6w
+# PZoB0BcC7SETNB8x2uKzJ2MyAIuyN0Uy/mGDeLyz9cSboKoG6aQibnjCnGAVOVn6
+# J7bvYWJsGu7HukMoTAIqC6oMGerNakhOCgrhU7m+cERPkTcADVH/PWhy+FJWd2px
+# ViKcyzWQSyX93PcOj2SsHvi7vEAfCGcxggH1MIIB8QIBATBUMD0xEzARBgoJkiaJ
+# k/IsZAEZFgNMQUIxFDASBgoJkiaJk/IsZAEZFgRaRVJPMRAwDgYDVQQDEwdaZXJv
+# U0NBAhNYAAAAPDajznxlIudFAAAAAAA8MAkGBSsOAwIaBQCgeDAYBgorBgEEAYI3
+# AgEMMQowCKACgAChAoAAMBkGCSqGSIb3DQEJAzEMBgorBgEEAYI3AgEEMBwGCisG
+# AQQBgjcCAQsxDjAMBgorBgEEAYI3AgEVMCMGCSqGSIb3DQEJBDEWBBSaisRAFdXx
+# spk7Lpz4jUbYHfM5dzANBgkqhkiG9w0BAQEFAASCAQAp9pkKbtIyLkyOo9LatOHQ
+# Z4xyUsdrGk05y4WXy/gpW+yZEA9MP6V55AF9eeBmWj7bfhh9hVMpuO/fwOWYwmbx
+# Nhjb8/LvBBZiQj5TkxF1uJs+6POHpsZO8vRCMckX931UB0ZGCcxTU/xm7Bw24sNZ
+# NpaNlwW+NTiIzOGD3jnhFl3fg8OL4jSuf6HOSLWsrm51gjwCjeeGXibl+gVfsBqE
+# Lc37wLN5BbrGJPUOw5OfKVoGuVn5ZH0l7NyD0ZPwvHZM/wQNUrG9eXvZ+aWYCEFc
+# gRnzqz+e0pMKZYcgJLNzs/LbtrLCz/kq+T0rFnA2RZWQmi6Mt7r4LbJ8Go79CgC5
+# SIG # End signature block
