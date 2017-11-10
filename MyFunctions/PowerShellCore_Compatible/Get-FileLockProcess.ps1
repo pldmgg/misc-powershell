e513eacf2eea8775ec15778b9bc276e84dd58f58
pldmgg <pldmgg@mykolab.com>
2017-11-10 12:54:26 -0500

    Initial commit for Get-FileLockProcess

---
+++ MyFunctions/PowerShellCore_Compatible/Get-FileLockProcess.ps1
@@ -0,0 +1,332 @@
+<#
+.SYNOPSIS
+    Check which process is locking a file
+
+.DESCRIPTION
+    Get-FileLock takes a path to a file and returns a System.Collections.Generic.List of System.Diagnostic.Process
+    objects (one or more processes could have a lock on a specific file, which is why a List is used).
+
+.NOTES
+    Solution credit to: https://stackoverflow.com/a/20623311
+
+.PARAMETER FilePath
+    This parameter is MANDATORY.
+
+    This parameter takes a string that represents a full path to a file.
+    
+.EXAMPLE
+    PS C:\Users\testadmin> Get-FileLockProcess -FilePath "$HOME\Downloads\call_activity_2017_Nov.xlsx"
+        
+    Handles  NPM(K)    PM(K)      WS(K)     CPU(s)     Id  SI ProcessName
+    -------  ------    -----      -----     ------     --  -- -----------
+    1074      51    50056      86984       5.86   2856   2 EXCEL
+#>
+
+function Get-FileLockProcess {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True)]
+        [string]$FilePath
+    )
+
+    ##### BEGIN Variable/Parameter Transforms and PreRun Prep #####
+
+    if (! $(Test-Path $FilePath)) {
+        Write-Error "The path $FilePath was not found! Halting!"
+        $global:FunctionResult = "1"
+        return
+    }
+
+
+
+    ##### END Variable/Parameter Transforms and PreRun Prep #####
+
+
+    ##### BEGIN Main Body #####
+
+    if ($PSVersionTable.PSEdition -eq "Desktop" -or $PSVersionTable.Platform -eq "Win32NT") {
+        $CurrentlyLoadedAssemblies = [System.AppDomain]::CurrentDomain.GetAssemblies()
+    
+        $AssembiesFullInfo = $CurrentlyLoadedAssemblies | Where-Object {
+            $_.GetName().Name -eq "Microsoft.CSharp"
+            $_.GetName().Name -eq "mscorlib"
+            $_.GetName().Name -eq "System"
+            $_.GetName().Name -eq "System.Collections"
+            $_.GetName().Name -eq "System.Core"
+            $_.GetName().Name -eq "System.IO"
+            $_.GetName().Name -eq "System.Linq"
+            $_.GetName().Name -eq "System.Runtime"
+            $_.GetName().Name -eq "System.Runtime.Extensions"
+            $_.GetName().Name -eq "System.Runtime.InteropServices"
+        }
+        $AssembiesFullInfo = $AssembiesFullInfo | Where-Object {$_.IsDynamic -eq $False}
+        
+        $usingStatementsAsString = @"
+        using Microsoft.CSharp;
+        using System.Collections.Generic;
+        using System.Collections;
+        using System.IO;
+        using System.Linq;
+        using System.Runtime.InteropServices;
+        using System.Runtime;
+        using System;
+        using System.Diagnostics;
+"@
+        
+        $ReferencedAssemblies = $AssembiesFullInfo.FullName | Sort-Object | Get-Unique
+        
+        # Using Type Extensions in PowerShell see: https://powershell.org/forums/topic/how-do-i-use-extension-methods-in-zipfileextensionsclass/
+        
+        $TypeDefinition = @"
+        $usingStatementsAsString
+        
+        namespace MyCore.Utils
+        {
+            static public class FileLockUtil
+            {
+                [StructLayout(LayoutKind.Sequential)]
+                struct RM_UNIQUE_PROCESS
+                {
+                    public int dwProcessId;
+                    public System.Runtime.InteropServices.ComTypes.FILETIME ProcessStartTime;
+                }
+        
+                const int RmRebootReasonNone = 0;
+                const int CCH_RM_MAX_APP_NAME = 255;
+                const int CCH_RM_MAX_SVC_NAME = 63;
+        
+                enum RM_APP_TYPE
+                {
+                    RmUnknownApp = 0,
+                    RmMainWindow = 1,
+                    RmOtherWindow = 2,
+                    RmService = 3,
+                    RmExplorer = 4,
+                    RmConsole = 5,
+                    RmCritical = 1000
+                }
+        
+                [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+                struct RM_PROCESS_INFO
+                {
+                    public RM_UNIQUE_PROCESS Process;
+        
+                    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_APP_NAME + 1)]
+                    public string strAppName;
+        
+                    [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCH_RM_MAX_SVC_NAME + 1)]
+                    public string strServiceShortName;
+        
+                    public RM_APP_TYPE ApplicationType;
+                    public uint AppStatus;
+                    public uint TSSessionId;
+                    [MarshalAs(UnmanagedType.Bool)]
+                    public bool bRestartable;
+                }
+        
+                [DllImport("rstrtmgr.dll", CharSet = CharSet.Unicode)]
+                static extern int RmRegisterResources(uint pSessionHandle,
+                                                    UInt32 nFiles,
+                                                    string[] rgsFilenames,
+                                                    UInt32 nApplications,
+                                                    [In] RM_UNIQUE_PROCESS[] rgApplications,
+                                                    UInt32 nServices,
+                                                    string[] rgsServiceNames);
+        
+                [DllImport("rstrtmgr.dll", CharSet = CharSet.Auto)]
+                static extern int RmStartSession(out uint pSessionHandle, int dwSessionFlags, string strSessionKey);
+        
+                [DllImport("rstrtmgr.dll")]
+                static extern int RmEndSession(uint pSessionHandle);
+        
+                [DllImport("rstrtmgr.dll")]
+                static extern int RmGetList(uint dwSessionHandle,
+                                            out uint pnProcInfoNeeded,
+                                            ref uint pnProcInfo,
+                                            [In, Out] RM_PROCESS_INFO[] rgAffectedApps,
+                                            ref uint lpdwRebootReasons);
+        
+                /// <summary>
+                /// Find out what process(es) have a lock on the specified file.
+                /// </summary>
+                /// <param name="path">Path of the file.</param>
+                /// <returns>Processes locking the file</returns>
+                /// <remarks>See also:
+                /// http://msdn.microsoft.com/en-us/library/windows/desktop/aa373661(v=vs.85).aspx
+                /// http://wyupdate.googlecode.com/svn-history/r401/trunk/frmFilesInUse.cs (no copyright in code at time of viewing)
+                /// 
+                /// </remarks>
+                static public List<Process> WhoIsLocking(string path)
+                {
+                    uint handle;
+                    string key = Guid.NewGuid().ToString();
+                    List<Process> processes = new List<Process>();
+        
+                    int res = RmStartSession(out handle, 0, key);
+                    if (res != 0) throw new Exception("Could not begin restart session.  Unable to determine file locker.");
+        
+                    try
+                    {
+                        const int ERROR_MORE_DATA = 234;
+                        uint pnProcInfoNeeded = 0,
+                            pnProcInfo = 0,
+                            lpdwRebootReasons = RmRebootReasonNone;
+        
+                        string[] resources = new string[] { path }; // Just checking on one resource.
+        
+                        res = RmRegisterResources(handle, (uint)resources.Length, resources, 0, null, 0, null);
+        
+                        if (res != 0) throw new Exception("Could not register resource.");                                    
+        
+                        //Note: there's a race condition here -- the first call to RmGetList() returns
+                        //      the total number of process. However, when we call RmGetList() again to get
+                        //      the actual processes this number may have increased.
+                        res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, null, ref lpdwRebootReasons);
+        
+                        if (res == ERROR_MORE_DATA)
+                        {
+                            // Create an array to store the process results
+                            RM_PROCESS_INFO[] processInfo = new RM_PROCESS_INFO[pnProcInfoNeeded];
+                            pnProcInfo = pnProcInfoNeeded;
+        
+                            // Get the list
+                            res = RmGetList(handle, out pnProcInfoNeeded, ref pnProcInfo, processInfo, ref lpdwRebootReasons);
+                            if (res == 0)
+                            {
+                                processes = new List<Process>((int)pnProcInfo);
+        
+                                // Enumerate all of the results and add them to the 
+                                // list to be returned
+                                for (int i = 0; i < pnProcInfo; i++)
+                                {
+                                    try
+                                    {
+                                        processes.Add(Process.GetProcessById(processInfo[i].Process.dwProcessId));
+                                    }
+                                    // catch the error -- in case the process is no longer running
+                                    catch (ArgumentException) { }
+                                }
+                            }
+                            else throw new Exception("Could not list processes locking resource.");                    
+                        }
+                        else if (res != 0) throw new Exception("Could not list processes locking resource. Failed to get size of result.");                    
+                    }
+                    finally
+                    {
+                        RmEndSession(handle);
+                    }
+        
+                    return processes;
+                }
+            }
+        }
+"@
+
+        $CheckMyCoreUtilsDownloadIdLoaded = $CurrentlyLoadedAssemblies | Where-Object {$_.ExportedTypes -like "MyCore.Utils.FileLockUtil*"}
+        if ($CheckMyCoreUtilsDownloadIdLoaded -eq $null) {
+            Add-Type -ReferencedAssemblies $ReferencedAssemblies -TypeDefinition $TypeDefinition
+        }
+        else {
+            Write-Verbose "The Namespace MyCore.Utils Class FileLockUtil is already loaded and available!"
+        }
+
+        [MyCore.Utils.FileLockUtil]::WhoIsLocking($FilePath)
+    }
+    if ($PSVersionTable.Platform -ne $null -and $PSVersionTable.Platform -ne "Win32NT") {
+        lsof $FilePath
+    }
+    
+    ##### END Main Body #####
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# SIG # Begin signature block
+# MIIMiAYJKoZIhvcNAQcCoIIMeTCCDHUCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUdlkcnLZnVTRAJuMlDZuebiXQ
+# Fzmgggn9MIIEJjCCAw6gAwIBAgITawAAAB/Nnq77QGja+wAAAAAAHzANBgkqhkiG
+# 9w0BAQsFADAwMQwwCgYDVQQGEwNMQUIxDTALBgNVBAoTBFpFUk8xETAPBgNVBAMT
+# CFplcm9EQzAxMB4XDTE3MDkyMDIxMDM1OFoXDTE5MDkyMDIxMTM1OFowPTETMBEG
+# CgmSJomT8ixkARkWA0xBQjEUMBIGCgmSJomT8ixkARkWBFpFUk8xEDAOBgNVBAMT
+# B1plcm9TQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDCwqv+ROc1
+# bpJmKx+8rPUUfT3kPSUYeDxY8GXU2RrWcL5TSZ6AVJsvNpj+7d94OEmPZate7h4d
+# gJnhCSyh2/3v0BHBdgPzLcveLpxPiSWpTnqSWlLUW2NMFRRojZRscdA+e+9QotOB
+# aZmnLDrlePQe5W7S1CxbVu+W0H5/ukte5h6gsKa0ktNJ6X9nOPiGBMn1LcZV/Ksl
+# lUyuTc7KKYydYjbSSv2rQ4qmZCQHqxyNWVub1IiEP7ClqCYqeCdsTtfw4Y3WKxDI
+# JaPmWzlHNs0nkEjvnAJhsRdLFbvY5C2KJIenxR0gA79U8Xd6+cZanrBUNbUC8GCN
+# wYkYp4A4Jx+9AgMBAAGjggEqMIIBJjASBgkrBgEEAYI3FQEEBQIDAQABMCMGCSsG
+# AQQBgjcVAgQWBBQ/0jsn2LS8aZiDw0omqt9+KWpj3DAdBgNVHQ4EFgQUicLX4r2C
+# Kn0Zf5NYut8n7bkyhf4wGQYJKwYBBAGCNxQCBAweCgBTAHUAYgBDAEEwDgYDVR0P
+# AQH/BAQDAgGGMA8GA1UdEwEB/wQFMAMBAf8wHwYDVR0jBBgwFoAUdpW6phL2RQNF
+# 7AZBgQV4tgr7OE0wMQYDVR0fBCowKDAmoCSgIoYgaHR0cDovL3BraS9jZXJ0ZGF0
+# YS9aZXJvREMwMS5jcmwwPAYIKwYBBQUHAQEEMDAuMCwGCCsGAQUFBzAChiBodHRw
+# Oi8vcGtpL2NlcnRkYXRhL1plcm9EQzAxLmNydDANBgkqhkiG9w0BAQsFAAOCAQEA
+# tyX7aHk8vUM2WTQKINtrHKJJi29HaxhPaHrNZ0c32H70YZoFFaryM0GMowEaDbj0
+# a3ShBuQWfW7bD7Z4DmNc5Q6cp7JeDKSZHwe5JWFGrl7DlSFSab/+a0GQgtG05dXW
+# YVQsrwgfTDRXkmpLQxvSxAbxKiGrnuS+kaYmzRVDYWSZHwHFNgxeZ/La9/8FdCir
+# MXdJEAGzG+9TwO9JvJSyoGTzu7n93IQp6QteRlaYVemd5/fYqBhtskk1zDiv9edk
+# mHHpRWf9Xo94ZPEy7BqmDuixm4LdmmzIcFWqGGMo51hvzz0EaE8K5HuNvNaUB/hq
+# MTOIB5145K8bFOoKHO4LkTCCBc8wggS3oAMCAQICE1gAAAH5oOvjAv3166MAAQAA
+# AfkwDQYJKoZIhvcNAQELBQAwPTETMBEGCgmSJomT8ixkARkWA0xBQjEUMBIGCgmS
+# JomT8ixkARkWBFpFUk8xEDAOBgNVBAMTB1plcm9TQ0EwHhcNMTcwOTIwMjE0MTIy
+# WhcNMTkwOTIwMjExMzU4WjBpMQswCQYDVQQGEwJVUzELMAkGA1UECBMCUEExFTAT
+# BgNVBAcTDFBoaWxhZGVscGhpYTEVMBMGA1UEChMMRGlNYWdnaW8gSW5jMQswCQYD
+# VQQLEwJJVDESMBAGA1UEAxMJWmVyb0NvZGUyMIIBIjANBgkqhkiG9w0BAQEFAAOC
+# AQ8AMIIBCgKCAQEAxX0+4yas6xfiaNVVVZJB2aRK+gS3iEMLx8wMF3kLJYLJyR+l
+# rcGF/x3gMxcvkKJQouLuChjh2+i7Ra1aO37ch3X3KDMZIoWrSzbbvqdBlwax7Gsm
+# BdLH9HZimSMCVgux0IfkClvnOlrc7Wpv1jqgvseRku5YKnNm1JD+91JDp/hBWRxR
+# 3Qg2OR667FJd1Q/5FWwAdrzoQbFUuvAyeVl7TNW0n1XUHRgq9+ZYawb+fxl1ruTj
+# 3MoktaLVzFKWqeHPKvgUTTnXvEbLh9RzX1eApZfTJmnUjBcl1tCQbSzLYkfJlJO6
+# eRUHZwojUK+TkidfklU2SpgvyJm2DhCtssFWiQIDAQABo4ICmjCCApYwDgYDVR0P
+# AQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMDMB0GA1UdDgQWBBS5d2bhatXq
+# eUDFo9KltQWHthbPKzAfBgNVHSMEGDAWgBSJwtfivYIqfRl/k1i63yftuTKF/jCB
+# 6QYDVR0fBIHhMIHeMIHboIHYoIHVhoGubGRhcDovLy9DTj1aZXJvU0NBKDEpLENO
+# PVplcm9TQ0EsQ049Q0RQLENOPVB1YmxpYyUyMEtleSUyMFNlcnZpY2VzLENOPVNl
+# cnZpY2VzLENOPUNvbmZpZ3VyYXRpb24sREM9emVybyxEQz1sYWI/Y2VydGlmaWNh
+# dGVSZXZvY2F0aW9uTGlzdD9iYXNlP29iamVjdENsYXNzPWNSTERpc3RyaWJ1dGlv
+# blBvaW50hiJodHRwOi8vcGtpL2NlcnRkYXRhL1plcm9TQ0EoMSkuY3JsMIHmBggr
+# BgEFBQcBAQSB2TCB1jCBowYIKwYBBQUHMAKGgZZsZGFwOi8vL0NOPVplcm9TQ0Es
+# Q049QUlBLENOPVB1YmxpYyUyMEtleSUyMFNlcnZpY2VzLENOPVNlcnZpY2VzLENO
+# PUNvbmZpZ3VyYXRpb24sREM9emVybyxEQz1sYWI/Y0FDZXJ0aWZpY2F0ZT9iYXNl
+# P29iamVjdENsYXNzPWNlcnRpZmljYXRpb25BdXRob3JpdHkwLgYIKwYBBQUHMAKG
+# Imh0dHA6Ly9wa2kvY2VydGRhdGEvWmVyb1NDQSgxKS5jcnQwPQYJKwYBBAGCNxUH
+# BDAwLgYmKwYBBAGCNxUIg7j0P4Sb8nmD8Y84g7C3MobRzXiBJ6HzzB+P2VUCAWQC
+# AQUwGwYJKwYBBAGCNxUKBA4wDDAKBggrBgEFBQcDAzANBgkqhkiG9w0BAQsFAAOC
+# AQEAszRRF+YTPhd9UbkJZy/pZQIqTjpXLpbhxWzs1ECTwtIbJPiI4dhAVAjrzkGj
+# DyXYWmpnNsyk19qE82AX75G9FLESfHbtesUXnrhbnsov4/D/qmXk/1KD9CE0lQHF
+# Lu2DvOsdf2mp2pjdeBgKMRuy4cZ0VCc/myO7uy7dq0CvVdXRsQC6Fqtr7yob9NbE
+# OdUYDBAGrt5ZAkw5YeL8H9E3JLGXtE7ir3ksT6Ki1mont2epJfHkO5JkmOI6XVtg
+# anuOGbo62885BOiXLu5+H2Fg+8ueTP40zFhfLh3e3Kj6Lm/NdovqqTBAsk04tFW9
+# Hp4gWfVc0gTDwok3rHOrfIY35TGCAfUwggHxAgEBMFQwPTETMBEGCgmSJomT8ixk
+# ARkWA0xBQjEUMBIGCgmSJomT8ixkARkWBFpFUk8xEDAOBgNVBAMTB1plcm9TQ0EC
+# E1gAAAH5oOvjAv3166MAAQAAAfkwCQYFKw4DAhoFAKB4MBgGCisGAQQBgjcCAQwx
+# CjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYBBAGC
+# NwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFIuVRne3mrTZCalE
+# 4soF0g7OtGHTMA0GCSqGSIb3DQEBAQUABIIBAIOwys4xA4P90Q2zM7CQtnC4xHs0
+# 6YMUPEBIlX5ZCn5ysvlvgarj3wHU+YzdLDfvr5Od5YrrkqD9903KbNQBd58zOyw1
+# j0e+4vklbG6CD8hGP/vGed4QXngm2ZFBnLU4jUYdrqmjWRR8RxLGb+80Nowvs1QT
+# O3X3sgGLqGpqnLiOCyPs63KO7JrbVqwJTnF++T+Oh/XLNDxJJI4ihujV0hpcQOn3
+# 0+Ir4+JhD7090nmj7quuBDWjSS6BtaO4D2YQyq4hHkr4fFwfhSv4KtMZgWWEUP6p
+# uro2uipvx2k56Kxz6WXwDncg2HT05Wq7DtCShpd0/Y97O3/QiK939yZkW+k=
+# SIG # End signature block
