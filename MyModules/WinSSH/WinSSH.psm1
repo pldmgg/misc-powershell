d1f48085430a28942eb76f660eab4458a6a23f60
pldmgg <pldmgg@mykolab.com>
2018-02-08 09:30:20 -0500

    Minor updates

--- MyModules/WinSSH/WinSSH.psm1
+++ MyModules/WinSSH/WinSSH.psm1
@@ -1157,21 +1157,251 @@ function Update-PackageManagement {
 }
 
 function Install-ChocolateyCmdLine {
-    # The below Install-Package Chocolatey screws up $env:Path, so restore it afterwards
-    $OriginalEnvPath = $env:Path
-    if (![bool]$(Get-Package -Name Chocolatey -ProviderName Chocolatey -ErrorAction SilentlyContinue)) {
-        Install-Package Chocolatey -Provider Chocolatey -Force
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory=$False)]
+        [switch]$NoUpdatePackageManagement
+    )
+
+    ##### BEGIN Variable/Parameter Transforms and PreRun Prep #####
+
+    Write-Host "Please wait..."
+    $global:FunctionResult = "0"
+    $MyFunctionsUrl = "https://raw.githubusercontent.com/pldmgg/misc-powershell/master/MyFunctions"
+
+    if (!$NoUpdatePackageManagement) {
+        if (![bool]$(Get-Command Update-PackageManagement -ErrorAction SilentlyContinue)) {
+            $UpdatePMFunctionUrl = "$MyFunctionsUrl/PowerShellCore_Compatible/Update-PackageManagement.ps1"
+            try {
+                Invoke-Expression $([System.Net.WebClient]::new().DownloadString($UpdatePMFunctionUrl))
+            }
+            catch {
+                Write-Error $_
+                Write-Error "Unable to load the Update-PackageManagement function! Halting!"
+                $global:FunctionResult = "1"
+                return
+            }
+        }
+
+        try {
+            $global:FunctionResult = "0"
+            $null = Update-PackageManagement -UseChocolatey -ErrorAction SilentlyContinue -ErrorVariable UPMErr
+            if ($UPMErr -and $global:FunctionResult -eq 1) {throw}
+        }
+        catch {
+            Write-Host "Errors from the Update-PackageManagement function are as follows:"
+            foreach ($error in $UPMErr) {Write-Error $($error | Out-String)}
+            Write-Error "The Update-PackageManagement function failed! Halting!"
+            $global:FunctionResult = "1"
+            return
+        }
     }
-    $env:Path = $OriginalEnvPath
+
+    if (![bool]$(Get-Command Refresh-ChocolateyEnv -ErrorAction SilentlyContinue)) {
+        $RefreshCEFunctionUrl = "$MyFunctionsUrl/PowerShellCore_Compatible/Refresh-ChocolateyEnv.ps1"
+        try {
+            Invoke-Expression $([System.Net.WebClient]::new().DownloadString($RefreshCEFunctionUrl))
+        }
+        catch {
+            Write-Error $_
+            Write-Error "Unable to load the Refresh-ChocolateyEnv function! Halting!"
+            $global:FunctionResult = "1"
+            return
+        }
+    }
+
+    ##### END Variable/Parameter Transforms and PreRun Prep #####
+
+
+    ##### BEGIN Main Body #####
+
     if (![bool]$(Get-Command choco -ErrorAction SilentlyContinue)) {
-        Write-Host "Refreshing `$env:Path ..."
-        Refresh-ChocolateyEnv
+        # The below Install-Package Chocolatey screws up $env:Path, so restore it afterwards
+        $OriginalEnvPath = $env:Path
+
+        # Installing Package Providers is spotty sometimes...Using while loop 3 times before failing
+        $Counter = 0
+        while ($(Get-PackageProvider).Name -notcontains "Chocolatey" -and $Counter -lt 3) {
+            Install-PackageProvider -Name Chocolatey -Force -Confirm:$false -WarningAction SilentlyContinue
+            $Counter++
+            Start-Sleep -Seconds 5
+        }
+        if ($(Get-PackageProvider).Name -notcontains "Chocolatey") {
+            Write-Error "Unable to install the Chocolatey Package Provider / Repo for PackageManagement/PowerShellGet! Halting!"
+            $global:FunctionResult = "1"
+            return
+        }
+
+        if (![bool]$(Get-Package -Name Chocolatey -ProviderName Chocolatey -ErrorAction SilentlyContinue)) {
+            # NOTE: The PackageManagement install of choco is unreliable, so just in case, fallback to the Chocolatey cmdline for install
+            $null = Install-Package Chocolatey -Provider Chocolatey -Force -Confirm:$false -ErrorVariable ChocoInstallError -ErrorAction SilentlyContinue -WarningAction SilentlyContinue
+            
+            if ($ChocoInstallError.Count -gt 0) {
+                Write-Warning "There was a problem installing the Chocolatey CmdLine via PackageManagement/PowerShellGet!"
+                $InstallViaOfficialScript = $True
+                Uninstall-Package Chocolatey -Force -ErrorAction SilentlyContinue
+            }
+
+            if ($ChocoInstallError.Count -eq 0) {
+                $PMPGetInstall = $True
+            }
+        }
+
+        # Try and find choco.exe
+        try {
+            Write-Host "Refreshing `$env:Path..."
+            $global:FunctionResult = "0"
+            $null = Refresh-ChocolateyEnv -ErrorAction SilentlyContinue -ErrorVariable RCEErr
+            
+            if ($RCEErr.Count -gt 0 -and
+            $global:FunctionResult -eq "1" -and
+            ![bool]$($RCEErr -match "Neither the Chocolatey PackageProvider nor the Chocolatey CmdLine appears to be installed!")) {
+                throw
+            }
+        }
+        catch {
+            Write-Host "Errors from the Refresh-ChocolateyEnv function are as follows:"
+            foreach ($error in $RCEErr) {Write-Error $($error | Out-String)}
+            Write-Error "The Refresh-ChocolateyEnv function failed! Halting!"
+            $global:FunctionResult = "1"
+            return
+        }
+
+        if ($PMPGetInstall) {
+            # It's possible that PowerShellGet didn't run the chocolateyInstall.ps1 script to actually install the
+            # Chocolatey CmdLine. So do it manually.
+            if (Test-Path "C:\Chocolatey") {
+                $ChocolateyPath = "C:\Chocolatey"
+            }
+            elseif (Test-Path "C:\ProgramData\chocolatey") {
+                $ChocolateyPath = "C:\ProgramData\chocolatey"
+            }
+            else {
+                Write-Warning "Unable to find Chocolatey directory! Halting!"
+                Write-Host "Installing via official script at https://chocolatey.org/install.ps1"
+                $InstallViaOfficialScript = $True
+            }
+            
+            if ($ChocolateyPath) {
+                $ChocolateyInstallScript = $(Get-ChildItem -Path $ChocolateyPath -Recurse -File -Filter "*chocolateyinstall.ps1").FullName | Where-Object {
+                    $_ -match ".*?chocolatey\.[0-9].*?chocolateyinstall.ps1$"
+                }
+
+                if (!$ChocolateyInstallScript) {
+                    Write-Warning "Unable to find chocolateyinstall.ps1!"
+                    $InstallViaOfficialScript = $True
+                }
+            }
+
+            if ($ChocolateyInstallScript) {
+                try {
+                    Write-Host "Trying PowerShellGet Chocolatey CmdLine install script from $ChocolateyInstallScript ..." -ForegroundColor Yellow
+                    & $ChocolateyInstallScript
+                }
+                catch {
+                    Write-Error $_
+                    Write-Error "The Chocolatey Install Script $ChocolateyInstallScript has failed!"
+
+                    if ([bool]$(Get-Package $ProgramName)) {
+                        Uninstall-Package Chocolatey -Force -ErrorAction SilentlyContinue
+                    }
+                }
+            }
+        }
+
+        # If we still can't find choco.exe, then use the Chocolatey install script from chocolatey.org
+        if (![bool]$(Get-Command choco -ErrorAction SilentlyContinue) -or $InstallViaOfficialScript) {
+            $ChocolateyInstallScriptUrl = "https://chocolatey.org/install.ps1"
+            try {
+                Invoke-Expression $([System.Net.WebClient]::new().DownloadString($ChocolateyInstallScriptUrl))
+            }
+            catch {
+                Write-Error $_
+                Write-Error "Unable to install Chocolatey via the official chocolatey.org script! Halting!"
+                $global:FunctionResult = "1"
+                return
+            }
+
+            $PMPGetInstall = $False
+        }
+        
+        # If we STILL can't find choco.exe, then Refresh-ChocolateyEnv a third time...
+        #if (![bool]$($env:Path -split ";" -match "chocolatey\\bin")) {
+        if (![bool]$(Get-Command choco -ErrorAction SilentlyContinue)) {
+            # ...and then find it again and add it to $env:Path via Refresh-ChocolateyEnv function
+            if (![bool]$(Get-Command choco -ErrorAction SilentlyContinue)) {
+                try {
+                    Write-Host "Refreshing `$env:Path..."
+                    $global:FunctionResult = "0"
+                    $null = Refresh-ChocolateyEnv -ErrorAction SilentlyContinue -ErrorVariable RCEErr
+                    
+                    if ($RCEErr.Count -gt 0 -and
+                    $global:FunctionResult -eq "1" -and
+                    ![bool]$($RCEErr -match "Neither the Chocolatey PackageProvider nor the Chocolatey CmdLine appears to be installed!")) {
+                        throw
+                    }
+                }
+                catch {
+                    Write-Host "Errors from the Refresh-ChocolateyEnv function are as follows:"
+                    foreach ($error in $RCEErr) {Write-Error $($error | Out-String)}
+                    Write-Error "The Refresh-ChocolateyEnv function failed! Halting!"
+                    $global:FunctionResult = "1"
+                    return
+                }
+            }
+        }
+
+        # If we STILL can't find choco.exe, then give up...
+        if (![bool]$(Get-Command choco -ErrorAction SilentlyContinue)) {
+            Write-Error "Unable to find choco.exe after install! Check your `$env:Path! Halting!"
+            $global:FunctionResult = "1"
+            return
+        }
+        else {
+            Write-Host "Finished installing Chocolatey CmdLine." -ForegroundColor Green
+
+            try {
+                cup chocolatey-core.extension -y
+            }
+            catch {
+                Write-Error "Installation of chocolatey-core.extension via the Chocolatey CmdLine failed! Halting!"
+                $global:FunctionResult = "1"
+                return
+            }
+
+            try {
+                Write-Host "Refreshing `$env:Path..."
+                $global:FunctionResult = "0"
+                $null = Refresh-ChocolateyEnv -ErrorAction SilentlyContinue -ErrorVariable RCEErr
+                if ($RCEErr.Count -gt 0 -and $global:FunctionResult -eq "1") {throw}
+            }
+            catch {
+                Write-Host "Errors from the Refresh-ChocolateyEnv function are as follows:"
+                foreach ($error in $RCEErr) {Write-Error $($error | Out-String)}
+                Write-Error "The Refresh-ChocolateyEnv function failed! Halting!"
+                $global:FunctionResult = "1"
+                return
+            }
+
+            $ChocoModulesThatRefreshEnvShouldHaveLoaded = @(
+                "chocolatey-core"
+                "chocolateyInstaller"
+                "chocolateyProfile"
+                "chocolateysetup"
+            )
+
+            foreach ($ModName in $ChocoModulesThatRefreshEnvShouldHaveLoaded) {
+                if ($(Get-Module).Name -contains $ModName) {
+                    Write-Host "The $ModName Module has been loaded from $($(Get-Module -Name $ModName).Path)" -ForegroundColor Green
+                }
+            }
+        }
     }
-    if (![bool]$(Get-Command choco -ErrorAction SilentlyContinue)) {
-        Write-Error "Unable to find choco.exe! Halting!"
-        $global:FunctionResult = "1"
-        return
+    else {
+        Write-Warning "The Chocolatey CmdLine is already installed!"
     }
+
+    ##### END Main Body #####
 }
 
 function Refresh-ChocolateyEnv {
@@ -3248,7 +3478,7 @@ function New-SSHKey {
         }
         
         if (!$RemovePrivateKey) {
-            Write-Host "It is now safe to delete the private key (i.e. $($PrivKey.FullName)) since it has been added to the." -ForegroundColor Yellow
+            Write-Host "It is now safe to delete the private key (i.e. $($PrivKey.FullName)) since it has been added to the SSH Agent Service." -ForegroundColor Yellow
         }
     }
     else {
@@ -3350,7 +3580,7 @@ function Add-PublicKeyToRemoteHost {
         $RemoteHostLocation = $RemoteHostNetworkInfo.IPAddressList[0]
     }
 
-    ssh $RemoteHostUserName@$RemoteHostLocation "echo '$PubKeyContent' >> ~/.ssh/authorized_keys"
+    ssh -t $RemoteHostUserName@$RemoteHostLocation "echo '$PubKeyContent' >> ~/.ssh/authorized_keys"
 
     ##### END Main Body #####
 }
@@ -3712,8 +3942,8 @@ key that has been added to .ssh/authorized_keys on the Remote Windows Host.
 # SIG # Begin signature block
 # MIIMiAYJKoZIhvcNAQcCoIIMeTCCDHUCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
 # gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
-# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUqBi38FyGE9DssIxVw4S9AhN/
-# UhWgggn9MIIEJjCCAw6gAwIBAgITawAAAB/Nnq77QGja+wAAAAAAHzANBgkqhkiG
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUY3oM8+iEr4PACB5tGliAnOdj
+# vYGgggn9MIIEJjCCAw6gAwIBAgITawAAAB/Nnq77QGja+wAAAAAAHzANBgkqhkiG
 # 9w0BAQsFADAwMQwwCgYDVQQGEwNMQUIxDTALBgNVBAoTBFpFUk8xETAPBgNVBAMT
 # CFplcm9EQzAxMB4XDTE3MDkyMDIxMDM1OFoXDTE5MDkyMDIxMTM1OFowPTETMBEG
 # CgmSJomT8ixkARkWA0xBQjEUMBIGCgmSJomT8ixkARkWBFpFUk8xEDAOBgNVBAMT
@@ -3770,11 +4000,11 @@ key that has been added to .ssh/authorized_keys on the Remote Windows Host.
 # ARkWA0xBQjEUMBIGCgmSJomT8ixkARkWBFpFUk8xEDAOBgNVBAMTB1plcm9TQ0EC
 # E1gAAAH5oOvjAv3166MAAQAAAfkwCQYFKw4DAhoFAKB4MBgGCisGAQQBgjcCAQwx
 # CjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYBBAGC
-# NwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFNlaYcbCB85WgK9S
-# 0Yrht2O2ATZ4MA0GCSqGSIb3DQEBAQUABIIBABb+Iuv4fRjwt7JIL5vjujT6mVGO
-# 6N/T6b1gdtN0SD/1xFj8YgJUI1aPJGdw0e4pMqKtAj4CjNV+59c3t95FgMJzvfHQ
-# 2aNS/FZu1q2J6ytOKmE4dziLkp+QcRSnszCYgFCh9VBwUUdfOu0edpUkRmxl/A4m
-# OPUcmoxMOwEBsv6xmY/NHRuPhBYCzmPf7qxsx3VliuKHYEbaAFBgPb3v7qWdERB1
-# JPq+sLTu8SUgiFBvCoj9O1cOhukvuwIclFX6WNivBthBOyM7VHwbbBrWCYHsFv94
-# 3hCwyA7dBLBtACJKhsdjIWdLRihHuNtSGLznm1N4IMgi2QzMg5pwGINPNtg=
+# NwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFBf29I3baOJ31NOk
+# 1fO9hd1rEpQjMA0GCSqGSIb3DQEBAQUABIIBAAjYaqxO5J0gFCtKtn3bI7CWUMGb
+# G+0f3M4psqVWFqDAyJSCE+UB9/s6Incn0zwsLq5hbWVA9GEZ0UZsC1rmXvriDsYv
+# 2QqIAgfnm0XO+eXcV6wCIP1NjzoFPIJGwjzBiK8rNwoFm13tglNhokWrBpjYch32
+# HbrqYnEtyZkd5wUB4Ul2E1aqi9yWFqKdIT2Ic+x29vbJBlDyHPENTbaA10yM+gL3
+# t+jHf4S9TcrbeN3D6T1jqHFaaie8Kx8vr37m54TgOrFXnNdyWAAit+3xqM3mCbFw
+# y1CYmMB9LgxRgv+nHEPT0qv/dweB9y/GME21QdX3duiqnmiDu/FRj0kypLs=
 # SIG # End signature block
