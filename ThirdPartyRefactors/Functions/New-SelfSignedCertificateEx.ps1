1814457e69fe0e4b0cfc3ef947e04186f5a7d065
pldmgg <pldmgg@mykolab.com>
2018-05-21 12:00:03 -0400

    Initial commit for New-SelfSignedCertificateEx.ps1 fix

---
+++ ThirdPartyRefactors/Functions/New-SelfSignedCertificateEx.ps1
@@ -0,0 +1,313 @@
+function New-SelfSignedCertificateEx {
+	[OutputType('[System.Security.Cryptography.X509Certificates.X509Certificate2]')]
+[	CmdletBinding(DefaultParameterSetName = '__store')]
+	param (
+		[Parameter(Mandatory = $true, Position = 0)]
+		[string]$Subject,
+		[Parameter(Position = 1)]
+		[datetime]$NotBefore = [DateTime]::Now.AddDays(-1),
+		[Parameter(Position = 2)]
+		[datetime]$NotAfter = $NotBefore.AddDays(365),
+		[string]$SerialNumber,
+		[Alias('CSP')]
+		[string]$ProviderName = "Microsoft Enhanced RSA and AES Cryptographic Provider",
+		[string]$AlgorithmName = "RSA",
+		[int]$KeyLength = 2048,
+		[validateSet("Exchange","Signature")]
+		[string]$KeySpec = "Exchange",
+		[Alias('EKU')]
+		[Security.Cryptography.Oid[]]$EnhancedKeyUsage,
+		[Alias('KU')]
+		[Security.Cryptography.X509Certificates.X509KeyUsageFlags]$KeyUsage,
+		[Alias('SAN')]
+		[String[]]$SubjectAlternativeName,
+		[bool]$IsCA,
+		[int]$PathLength = -1,
+		[Security.Cryptography.X509Certificates.X509ExtensionCollection]$CustomExtension,
+		[ValidateSet('MD5','SHA1','SHA256','SHA384','SHA512')]
+		[string]$SignatureAlgorithm = "SHA256",
+		[string]$FriendlyName,
+		[Parameter(ParameterSetName = '__store')]
+		[Security.Cryptography.X509Certificates.StoreLocation]$StoreLocation = "CurrentUser",
+		[Parameter(Mandatory = $true, ParameterSetName = '__file')]
+		[Alias('OutFile','OutPath','Out')]
+		[IO.FileInfo]$Path,
+		[Parameter(Mandatory = $true, ParameterSetName = '__file')]
+		[Security.SecureString]$Password,
+		[switch]$AllowSMIME,
+		[switch]$Exportable
+	)
+	$ErrorActionPreference = "Stop"
+	if ([Environment]::OSVersion.Version.Major -lt 6) {
+		$NotSupported = New-Object NotSupportedException -ArgumentList "Windows XP and Windows Server 2003 are not supported!"
+		throw $NotSupported
+	}
+	$ExtensionsToAdd = @()
+
+	#region constants
+	# contexts
+	New-Variable -Name UserContext -Value 0x1 -Option Constant
+	New-Variable -Name MachineContext -Value 0x2 -Option Constant
+	# encoding
+	New-Variable -Name Base64Header -Value 0x0 -Option Constant
+	New-Variable -Name Base64 -Value 0x1 -Option Constant
+	New-Variable -Name Binary -Value 0x3 -Option Constant
+	New-Variable -Name Base64RequestHeader -Value 0x4 -Option Constant
+	# SANs
+	New-Variable -Name OtherName -Value 0x1 -Option Constant
+	New-Variable -Name RFC822Name -Value 0x2 -Option Constant
+	New-Variable -Name DNSName -Value 0x3 -Option Constant
+	New-Variable -Name DirectoryName -Value 0x5 -Option Constant
+	New-Variable -Name URL -Value 0x7 -Option Constant
+	New-Variable -Name IPAddress -Value 0x8 -Option Constant
+	New-Variable -Name RegisteredID -Value 0x9 -Option Constant
+	New-Variable -Name Guid -Value 0xa -Option Constant
+	New-Variable -Name UPN -Value 0xb -Option Constant
+	# installation options
+	New-Variable -Name AllowNone -Value 0x0 -Option Constant
+	New-Variable -Name AllowNoOutstandingRequest -Value 0x1 -Option Constant
+	New-Variable -Name AllowUntrustedCertificate -Value 0x2 -Option Constant
+	New-Variable -Name AllowUntrustedRoot -Value 0x4 -Option Constant
+	# PFX export options
+	New-Variable -Name PFXExportEEOnly -Value 0x0 -Option Constant
+	New-Variable -Name PFXExportChainNoRoot -Value 0x1 -Option Constant
+	New-Variable -Name PFXExportChainWithRoot -Value 0x2 -Option Constant
+	#endregion
+
+	# http://msdn.microsoft.com/en-us/library/aa377051(VS.85).aspx
+	$SubjectDN = New-Object -ComObject X509Enrollment.CX500DistinguishedName
+	$SubjectDN.Encode($Subject, 0x0)
+	
+	#region Extensions
+
+	#region Enhanced Key Usages processing
+	if ($EnhancedKeyUsage) {
+		$OIDs = New-Object -ComObject X509Enrollment.CObjectIDs
+		$EnhancedKeyUsage | ForEach-Object {
+			$OID = New-Object -ComObject X509Enrollment.CObjectID
+			$OID.InitializeFromValue($_.Value)
+			# http://msdn.microsoft.com/en-us/library/aa376785(VS.85).aspx
+			$OIDs.Add($OID)
+		}
+		# http://msdn.microsoft.com/en-us/library/aa378132(VS.85).aspx
+		$EKU = New-Object -ComObject X509Enrollment.CX509ExtensionEnhancedKeyUsage
+		$EKU.InitializeEncode($OIDs)
+		$ExtensionsToAdd += "EKU"
+	}
+	#endregion
+
+	#region Key Usages processing
+	if ($KeyUsage -ne $null) {
+		$KU = New-Object -ComObject X509Enrollment.CX509ExtensionKeyUsage
+		$KU.InitializeEncode([int]$KeyUsage)
+		$KU.Critical = $true
+		$ExtensionsToAdd += "KU"
+	}
+	#endregion
+
+	#region Basic Constraints processing
+	if ($PSBoundParameters.Keys.Contains("IsCA")) {
+		# http://msdn.microsoft.com/en-us/library/aa378108(v=vs.85).aspx
+		$BasicConstraints = New-Object -ComObject X509Enrollment.CX509ExtensionBasicConstraints
+		if (!$IsCA) {$PathLength = -1}
+		$BasicConstraints.InitializeEncode($IsCA,$PathLength)
+		$BasicConstraints.Critical = $IsCA
+		$ExtensionsToAdd += "BasicConstraints"
+	}
+	#endregion
+
+	#region SAN processing
+	if ($SubjectAlternativeName) {
+		$SAN = New-Object -ComObject X509Enrollment.CX509ExtensionAlternativeNames
+		$Names = New-Object -ComObject X509Enrollment.CAlternativeNames
+		foreach ($altname in $SubjectAlternativeName) {
+			$Name = New-Object -ComObject X509Enrollment.CAlternativeName
+			switch -Regex ($altname) {
+				"^dns:(.+)" {$Name.InitializeFromString($DNSName,$Matches[1])}
+				"^email:(.+)" {$Name.InitializeFromString($RFC822Name,$Matches[1])}
+				"^upn:(.+)" {$Name.InitializeFromString($UPN,$Matches[1])}
+				"^ip:(.+)" {
+					$Bytes = [Net.IPAddress]::Parse($Matches[1]).GetAddressBytes()
+					$Name.InitializeFromRawData($IPAddress,$Base64,[Convert]::ToBase64String($Bytes))
+				}
+				"^dn:(.+)" {
+					$Bytes = ([Security.Cryptography.X509Certificates.X500DistinguishedName]$Matches[1]).RawData
+					$Name.InitializeFromRawData($DirectoryName,$Base64,[Convert]::ToBase64String($Bytes))
+				}
+				"^oid:(.+)" {$Name.InitializeFromString($RegisteredID,$Matches[1])}
+				"^url:(.+)" {$Name.InitializeFromString($URL,$Matches[1])}
+				"^guid:(.+)" {
+					$Bytes = [Guid]::Parse($Matches[1]).ToByteArray()
+					$Name.InitializeFromRawData($Guid,$Base64,[Convert]::ToBase64String($Bytes))
+				}
+				"other:(.+):(.+)" {
+					$Name.InitializeFromOtherName($matches[1],$base64,$Matches[2],$false)
+				}
+			}
+			$Names.Add($Name)
+		}
+		$SAN.InitializeEncode($Names)
+		$ExtensionsToAdd += "SAN"
+	}
+	#endregion
+
+	#region Custom Extensions
+	if ($CustomExtension) {
+		$count = 0
+		foreach ($ext in $CustomExtension) {
+			# http://msdn.microsoft.com/en-us/library/aa378077(v=vs.85).aspx
+			$Extension = New-Object -ComObject X509Enrollment.CX509Extension
+			$EOID = New-Object -ComObject X509Enrollment.CObjectId
+			$EOID.InitializeFromValue($ext.Oid.Value)
+			$EValue = [Convert]::ToBase64String($ext.RawData)
+			$Extension.Initialize($EOID,$Base64,$EValue)
+			$Extension.Critical = $ext.Critical
+			New-Variable -Name ("ext" + $count) -Value $Extension
+			$ExtensionsToAdd += ("ext" + $count)
+			$count++
+		}
+	}
+	#endregion
+
+	#endregion
+
+	#region Private Key
+	# http://msdn.microsoft.com/en-us/library/aa378921(VS.85).aspx
+	$PrivateKey = New-Object -ComObject X509Enrollment.CX509PrivateKey
+	$PrivateKey.ProviderName = $ProviderName
+	$AlgID = New-Object -ComObject X509Enrollment.CObjectId
+	$AlgID.InitializeFromValue(([Security.Cryptography.Oid]$AlgorithmName).Value)
+	$PrivateKey.Algorithm = $AlgID
+	# http://msdn.microsoft.com/en-us/library/aa379409(VS.85).aspx
+	$PrivateKey.KeySpec = switch ($KeySpec) {"Exchange" {1}; "Signature" {2}}
+	$PrivateKey.Length = $KeyLength
+	# key will be stored in current user certificate store
+	switch ($PSCmdlet.ParameterSetName) {
+		'__store' {
+			$PrivateKey.MachineContext = if ($StoreLocation -eq "LocalMachine") {$true} else {$false}
+		}
+		'__file' {
+			$PrivateKey.MachineContext = $false
+		}
+	}
+	$PrivateKey.ExportPolicy = if ($Exportable) {1} else {0}
+	$PrivateKey.Create()
+	#endregion
+
+	# http://msdn.microsoft.com/en-us/library/aa377124(VS.85).aspx
+	$Cert = New-Object -ComObject X509Enrollment.CX509CertificateRequestCertificate
+	if ($PrivateKey.MachineContext) {
+		$Cert.InitializeFromPrivateKey($MachineContext,$PrivateKey,"")
+	} else {
+		$Cert.InitializeFromPrivateKey($UserContext,$PrivateKey,"")
+	}
+	#region Subject processing
+
+	#endregion
+	$Cert.Subject = $SubjectDN
+	$Cert.Issuer = $Cert.Subject
+	$Cert.NotBefore = $NotBefore
+	$Cert.NotAfter = $NotAfter
+	foreach ($item in $ExtensionsToAdd) {$Cert.X509Extensions.Add((Get-Variable -Name $item -ValueOnly))}
+	if (![string]::IsNullOrEmpty($SerialNumber)) {
+		if ($SerialNumber -match "[^0-9a-fA-F]") {throw "Invalid serial number specified."}
+		if ($SerialNumber.Length % 2) {$SerialNumber = "0" + $SerialNumber}
+		$Bytes = $SerialNumber -split "(.{2})" | Where-Object {$_} | ForEach-Object{[Convert]::ToByte($_,16)}
+		$ByteString = [Convert]::ToBase64String($Bytes)
+		$Cert.SerialNumber.InvokeSet($ByteString,1)
+	}
+	if ($AllowSMIME) {$Cert.SmimeCapabilities = $true}
+	$SigOID = New-Object -ComObject X509Enrollment.CObjectId
+	$SigOID.InitializeFromValue(([Security.Cryptography.Oid]$SignatureAlgorithm).Value)
+	$Cert.SignatureInformation.HashAlgorithm = $SigOID
+	# completing certificate request template building
+	$Cert.Encode()
+	
+	# interface: http://msdn.microsoft.com/en-us/library/aa377809(VS.85).aspx
+	$Request = New-Object -ComObject X509Enrollment.CX509enrollment
+	$Request.InitializeFromRequest($Cert)
+	$Request.CertificateFriendlyName = $FriendlyName
+	$endCert = $Request.CreateRequest($Base64)
+	$Request.InstallResponse($AllowUntrustedCertificate,$endCert,$Base64,"")
+	switch ($PSCmdlet.ParameterSetName) {
+		'__file' {
+			$PFXString = $Request.CreatePFX(
+				[Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR($Password)),
+				$PFXExportEEOnly,
+				$Base64
+			)
+			Set-Content -Path $Path -Value ([Convert]::FromBase64String($PFXString)) -Encoding Byte
+		}
+	}
+	[Byte[]]$CertBytes = [Convert]::FromBase64String($endCert)
+	New-Object Security.Cryptography.X509Certificates.X509Certificate2 @(,$CertBytes)
+}
+# SIG # Begin signature block
+# MIIMiAYJKoZIhvcNAQcCoIIMeTCCDHUCAQExCzAJBgUrDgMCGgUAMGkGCisGAQQB
+# gjcCAQSgWzBZMDQGCisGAQQBgjcCAR4wJgIDAQAABBAfzDtgWUsITrck0sYpfvNR
+# AgEAAgEAAgEAAgEAAgEAMCEwCQYFKw4DAhoFAAQUaLgIUVIEOrHm+lhwPddUKHa5
+# kWygggn9MIIEJjCCAw6gAwIBAgITawAAAB/Nnq77QGja+wAAAAAAHzANBgkqhkiG
+# 9w0BAQsFADAwMQwwCgYDVQQGEwNMQUIxDTALBgNVBAoTBFpFUk8xETAPBgNVBAMT
+# CFplcm9EQzAxMB4XDTE3MDkyMDIxMDM1OFoXDTE5MDkyMDIxMTM1OFowPTETMBEG
+# CgmSJomT8ixkARkWA0xBQjEUMBIGCgmSJomT8ixkARkWBFpFUk8xEDAOBgNVBAMT
+# B1plcm9TQ0EwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDCwqv+ROc1
+# bpJmKx+8rPUUfT3kPSUYeDxY8GXU2RrWcL5TSZ6AVJsvNpj+7d94OEmPZate7h4d
+# gJnhCSyh2/3v0BHBdgPzLcveLpxPiSWpTnqSWlLUW2NMFRRojZRscdA+e+9QotOB
+# aZmnLDrlePQe5W7S1CxbVu+W0H5/ukte5h6gsKa0ktNJ6X9nOPiGBMn1LcZV/Ksl
+# lUyuTc7KKYydYjbSSv2rQ4qmZCQHqxyNWVub1IiEP7ClqCYqeCdsTtfw4Y3WKxDI
+# JaPmWzlHNs0nkEjvnAJhsRdLFbvY5C2KJIenxR0gA79U8Xd6+cZanrBUNbUC8GCN
+# wYkYp4A4Jx+9AgMBAAGjggEqMIIBJjASBgkrBgEEAYI3FQEEBQIDAQABMCMGCSsG
+# AQQBgjcVAgQWBBQ/0jsn2LS8aZiDw0omqt9+KWpj3DAdBgNVHQ4EFgQUicLX4r2C
+# Kn0Zf5NYut8n7bkyhf4wGQYJKwYBBAGCNxQCBAweCgBTAHUAYgBDAEEwDgYDVR0P
+# AQH/BAQDAgGGMA8GA1UdEwEB/wQFMAMBAf8wHwYDVR0jBBgwFoAUdpW6phL2RQNF
+# 7AZBgQV4tgr7OE0wMQYDVR0fBCowKDAmoCSgIoYgaHR0cDovL3BraS9jZXJ0ZGF0
+# YS9aZXJvREMwMS5jcmwwPAYIKwYBBQUHAQEEMDAuMCwGCCsGAQUFBzAChiBodHRw
+# Oi8vcGtpL2NlcnRkYXRhL1plcm9EQzAxLmNydDANBgkqhkiG9w0BAQsFAAOCAQEA
+# tyX7aHk8vUM2WTQKINtrHKJJi29HaxhPaHrNZ0c32H70YZoFFaryM0GMowEaDbj0
+# a3ShBuQWfW7bD7Z4DmNc5Q6cp7JeDKSZHwe5JWFGrl7DlSFSab/+a0GQgtG05dXW
+# YVQsrwgfTDRXkmpLQxvSxAbxKiGrnuS+kaYmzRVDYWSZHwHFNgxeZ/La9/8FdCir
+# MXdJEAGzG+9TwO9JvJSyoGTzu7n93IQp6QteRlaYVemd5/fYqBhtskk1zDiv9edk
+# mHHpRWf9Xo94ZPEy7BqmDuixm4LdmmzIcFWqGGMo51hvzz0EaE8K5HuNvNaUB/hq
+# MTOIB5145K8bFOoKHO4LkTCCBc8wggS3oAMCAQICE1gAAAH5oOvjAv3166MAAQAA
+# AfkwDQYJKoZIhvcNAQELBQAwPTETMBEGCgmSJomT8ixkARkWA0xBQjEUMBIGCgmS
+# JomT8ixkARkWBFpFUk8xEDAOBgNVBAMTB1plcm9TQ0EwHhcNMTcwOTIwMjE0MTIy
+# WhcNMTkwOTIwMjExMzU4WjBpMQswCQYDVQQGEwJVUzELMAkGA1UECBMCUEExFTAT
+# BgNVBAcTDFBoaWxhZGVscGhpYTEVMBMGA1UEChMMRGlNYWdnaW8gSW5jMQswCQYD
+# VQQLEwJJVDESMBAGA1UEAxMJWmVyb0NvZGUyMIIBIjANBgkqhkiG9w0BAQEFAAOC
+# AQ8AMIIBCgKCAQEAxX0+4yas6xfiaNVVVZJB2aRK+gS3iEMLx8wMF3kLJYLJyR+l
+# rcGF/x3gMxcvkKJQouLuChjh2+i7Ra1aO37ch3X3KDMZIoWrSzbbvqdBlwax7Gsm
+# BdLH9HZimSMCVgux0IfkClvnOlrc7Wpv1jqgvseRku5YKnNm1JD+91JDp/hBWRxR
+# 3Qg2OR667FJd1Q/5FWwAdrzoQbFUuvAyeVl7TNW0n1XUHRgq9+ZYawb+fxl1ruTj
+# 3MoktaLVzFKWqeHPKvgUTTnXvEbLh9RzX1eApZfTJmnUjBcl1tCQbSzLYkfJlJO6
+# eRUHZwojUK+TkidfklU2SpgvyJm2DhCtssFWiQIDAQABo4ICmjCCApYwDgYDVR0P
+# AQH/BAQDAgeAMBMGA1UdJQQMMAoGCCsGAQUFBwMDMB0GA1UdDgQWBBS5d2bhatXq
+# eUDFo9KltQWHthbPKzAfBgNVHSMEGDAWgBSJwtfivYIqfRl/k1i63yftuTKF/jCB
+# 6QYDVR0fBIHhMIHeMIHboIHYoIHVhoGubGRhcDovLy9DTj1aZXJvU0NBKDEpLENO
+# PVplcm9TQ0EsQ049Q0RQLENOPVB1YmxpYyUyMEtleSUyMFNlcnZpY2VzLENOPVNl
+# cnZpY2VzLENOPUNvbmZpZ3VyYXRpb24sREM9emVybyxEQz1sYWI/Y2VydGlmaWNh
+# dGVSZXZvY2F0aW9uTGlzdD9iYXNlP29iamVjdENsYXNzPWNSTERpc3RyaWJ1dGlv
+# blBvaW50hiJodHRwOi8vcGtpL2NlcnRkYXRhL1plcm9TQ0EoMSkuY3JsMIHmBggr
+# BgEFBQcBAQSB2TCB1jCBowYIKwYBBQUHMAKGgZZsZGFwOi8vL0NOPVplcm9TQ0Es
+# Q049QUlBLENOPVB1YmxpYyUyMEtleSUyMFNlcnZpY2VzLENOPVNlcnZpY2VzLENO
+# PUNvbmZpZ3VyYXRpb24sREM9emVybyxEQz1sYWI/Y0FDZXJ0aWZpY2F0ZT9iYXNl
+# P29iamVjdENsYXNzPWNlcnRpZmljYXRpb25BdXRob3JpdHkwLgYIKwYBBQUHMAKG
+# Imh0dHA6Ly9wa2kvY2VydGRhdGEvWmVyb1NDQSgxKS5jcnQwPQYJKwYBBAGCNxUH
+# BDAwLgYmKwYBBAGCNxUIg7j0P4Sb8nmD8Y84g7C3MobRzXiBJ6HzzB+P2VUCAWQC
+# AQUwGwYJKwYBBAGCNxUKBA4wDDAKBggrBgEFBQcDAzANBgkqhkiG9w0BAQsFAAOC
+# AQEAszRRF+YTPhd9UbkJZy/pZQIqTjpXLpbhxWzs1ECTwtIbJPiI4dhAVAjrzkGj
+# DyXYWmpnNsyk19qE82AX75G9FLESfHbtesUXnrhbnsov4/D/qmXk/1KD9CE0lQHF
+# Lu2DvOsdf2mp2pjdeBgKMRuy4cZ0VCc/myO7uy7dq0CvVdXRsQC6Fqtr7yob9NbE
+# OdUYDBAGrt5ZAkw5YeL8H9E3JLGXtE7ir3ksT6Ki1mont2epJfHkO5JkmOI6XVtg
+# anuOGbo62885BOiXLu5+H2Fg+8ueTP40zFhfLh3e3Kj6Lm/NdovqqTBAsk04tFW9
+# Hp4gWfVc0gTDwok3rHOrfIY35TGCAfUwggHxAgEBMFQwPTETMBEGCgmSJomT8ixk
+# ARkWA0xBQjEUMBIGCgmSJomT8ixkARkWBFpFUk8xEDAOBgNVBAMTB1plcm9TQ0EC
+# E1gAAAH5oOvjAv3166MAAQAAAfkwCQYFKw4DAhoFAKB4MBgGCisGAQQBgjcCAQwx
+# CjAIoAKAAKECgAAwGQYJKoZIhvcNAQkDMQwGCisGAQQBgjcCAQQwHAYKKwYBBAGC
+# NwIBCzEOMAwGCisGAQQBgjcCARUwIwYJKoZIhvcNAQkEMRYEFKiGVixsQWRBw7ha
+# EuEk1uLo7MkAMA0GCSqGSIb3DQEBAQUABIIBAGl+ZyL0ZKA2wzGwWUIc0Xj36+1O
+# VcXwN7j0R1fW93ffW2jm1L02a+PunVgQnkoNBFa4gP+RLxBrWD3sa2i41Oo9aRkO
+# fce2wrwcWYs4x58l5d5Ds4juL5dSyntBzbNJtAqNcDTS+BEJfAdGV+7Rlcv22JlA
+# GjVJKA/nqVl2Fh1GHTtw9bgTftLfDA/FXbQtlnjSXyTaU4K+qQ0gB/eCQnlZNQqj
+# zbPqaE04vSQY/xN9wQrnkAAV/AsOrroEVAlZEFc6CiLiAHeBHVpUYWMk9bHclC+E
+# 9FWKqDstHbTiWT/7V3F9Dxv0SxjsB9NEToX4uTFmhqkAPMz3GfZDcWuCsoc=
+# SIG # End signature block
