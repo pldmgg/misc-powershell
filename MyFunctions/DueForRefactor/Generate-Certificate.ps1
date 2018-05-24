66f397a3a8b44425e4ace3d5c6de4f77f48b3f50
pldmgg <pldmgg@mykolab.com>
2018-05-23 22:49:49 -0400

    Significant updates

--- MyFunctions/DueForRefactor/Generate-Certificate.ps1
+++ MyFunctions/DueForRefactor/Generate-Certificate.ps1
@@ -2600,6 +2600,32 @@ function Generate-Certificate {
     
         $Output
     }
+
+    function NewUniqueString {
+        [CmdletBinding()]
+        Param(
+            [Parameter(Mandatory=$False)]
+            [string[]]$ArrayOfStrings,
+    
+            [Parameter(Mandatory=$True)]
+            [string]$PossibleNewUniqueString
+        )
+    
+        if (!$ArrayOfStrings -or $ArrayOfStrings.Count -eq 0 -or ![bool]$($ArrayOfStrings -match "[\w]")) {
+            $PossibleNewUniqueString
+        }
+        else {
+            $OriginalString = $PossibleNewUniqueString
+            $Iteration = 1
+            while ($ArrayOfStrings -contains $PossibleNewUniqueString) {
+                $AppendedValue = "_$Iteration"
+                $PossibleNewUniqueString = $OriginalString + $AppendedValue
+                $Iteration++
+            }
+    
+            $PossibleNewUniqueString
+        }
+    }
     
     #endregion >> Libraries and Helper Functions
     
