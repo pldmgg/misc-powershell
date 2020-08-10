69c5fd3e2bb100c20e3621a9a3bb209b13d178dd
pldmgg <pldmgg@mykolab.com>
2020-08-10 14:53:09 -0400

    various updates

--- MyModules/SplashServer/SplashServer.psm1
+++ MyModules/SplashServer/SplashServer.psm1
@@ -1,3 +1,29 @@
+function NewUniqueString {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$False)]
+        [string[]]$ArrayOfStrings,
+
+        [Parameter(Mandatory=$True)]
+        [string]$PossibleNewUniqueString
+    )
+
+    if (!$ArrayOfStrings -or $ArrayOfStrings.Count -eq 0 -or ![bool]$($ArrayOfStrings -match "[\w]")) {
+        $PossibleNewUniqueString
+    }
+    else {
+        $OriginalString = $PossibleNewUniqueString
+        $Iteration = 1
+        while ($ArrayOfStrings -contains $PossibleNewUniqueString) {
+            $AppendedValue = "_$Iteration"
+            $PossibleNewUniqueString = $OriginalString + $AppendedValue
+            $Iteration++
+        }
+
+        $PossibleNewUniqueString
+    }
+}
+
 function Install-DotNetScript {
     [CmdletBinding()]
     Param ()
