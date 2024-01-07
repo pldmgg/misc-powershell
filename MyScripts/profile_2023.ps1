790b90df5de112c62518f816cebf0c6c209e7630
Paul DiMaggio <pauldimaggioofficial@gmail.com>
2024-01-07 13:59:11 -0500

    Updated profile_2023.ps1

--- MyScripts/profile_2023.ps1
+++ MyScripts/profile_2023.ps1
@@ -148,3 +148,39 @@ function Cache-SudoPwd {
 Set-Alias -Name "presudo" -Value Cache-SudoPwd
 function secureprompt {Read-Host 'Enter sudo password' -AsSecureString}
 function presudo {Cache-SudoPwd -SudoPass $(Read-Host 'Enter sudo password' -AsSecureString)}
+
+
+function Process-ICM {
+    [CmdletBinding()]
+    Param(
+        [Parameter(Mandatory=$True)]
+        [System.Management.Automation.Runspaces.PSSession]$PSSession,
+
+        [Parameter(Mandatory=$True)]
+        [string]$Command
+    )
+
+    # Validate [string]$Command has a line in it that indicates $OutputThatICareAbout is below here...
+    $OutputIndicatorLine = ($Command -split "`n") -match 'SuccessOutput'
+    if (!$OutputIndicatorLine) {
+        Write-Error "The Command you provided does not have a line with the string 'SuccessOutput' (including single quotes)`
+        that indicates where the output you care about is. Halting!"
+        return
+    }
+
+    Invoke-Command -Session $PSSession {Invoke-Expression -Command $using:Command} -ErrorVariable icmErrs 2>&1 | Tee-Object -Variable icmAllOutput *>$null
+
+    $ErrsThatICareAbout = $icmErrs.Exception.Message -notmatch '^NotSpecified'
+    #if ($ErrsThatICareAbout.Count -gt 0) {$ErrsThatICareAbout | foreach {Write-Error $_}}
+    $OutputThatICareAbout = $icmAllOutput[($icmAllOutput.IndexOf('SuccessOutput') + 1)..$icmAllOutput.Count]
+    #if ($OutputThatICareAbout.Count -gt 0) {$OutputThatICareAbout | foreach {$_}}
+
+    [pscustomobject]@{
+        Errors = $icmErrs
+        Output = $icmAllOutput
+        RealErrors = $ErrsThatICareAbout
+        RealOutput = $OutputThatICareAbout
+    }
+
+    Write-Host $OutputThatICareAbout
+}
