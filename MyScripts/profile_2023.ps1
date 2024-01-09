97d4353c8082ba500e6a8f5d02d32d289ab3773e
Paul DiMaggio <pauldimaggioofficial@gmail.com>
2024-01-09 11:39:13 -0500

    Updated Get-Elevation.ps1 linux_pwsh_profile_2023.ps1 and profile_2023.ps1

--- MyScripts/profile_2023.ps1
+++ MyScripts/profile_2023.ps1
@@ -217,3 +217,30 @@ function Process-ICM {
         }
     }
 }
+
+
+function Get-Elevation {
+    if ($PSVersionTable.PSEdition -eq "Desktop" -or $PSVersionTable.Platform -eq "Win32NT" -or $PSVersionTable.PSVersion.Major -le 5) {
+        [System.Security.Principal.WindowsPrincipal]$currentPrincipal = New-Object System.Security.Principal.WindowsPrincipal(
+            [System.Security.Principal.WindowsIdentity]::GetCurrent()
+        )
+  
+        [System.Security.Principal.WindowsBuiltInRole]$administratorsRole = [System.Security.Principal.WindowsBuiltInRole]::Administrator
+  
+        if($currentPrincipal.IsInRole($administratorsRole)) {
+            return $true
+        }
+        else {
+            return $false
+        }
+    }
+    
+    if ($PSVersionTable.Platform -eq "Unix") {
+        if ($(whoami) -eq "root") {
+            return $true
+        }
+        else {
+            return $false
+        }
+    }
+}
